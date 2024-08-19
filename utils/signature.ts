d86e3c7d3f9cf5e6656edd1042f99553e0ce7a83
kws <kws1207@kaist.ac.kr>
2024-08-19 22:34:03 +0900

    solve: Delegation

---
+++ utils/signature.ts
@@ -0,0 +1,12 @@
+import { keccak256 } from "js-sha3";
+
+export function getFunctionSignature(
+  functionName: string,
+  paramTypes: string[]
+): string {
+  const functionSignature = `${functionName}(${paramTypes.join(",")})`;
+  const hash = keccak256(functionSignature);
+  return `0x${hash.slice(0, 8)}`;
+}
+
+// console.log(getFunctionSignature("pwn", []));
